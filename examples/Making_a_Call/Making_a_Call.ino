db3897fce4f4589c7975742ceed25b470309ffa3
Kerem Akçora <77489853+kerem-ce@users.noreply.github.com>
2022-07-23 22:09:04 +0300

    Update Making_a_Call.ino

--- examples/Making_a_Call/Making_a_Call.ino
+++ examples/Making_a_Call/Making_a_Call.ino
@@ -4,12 +4,10 @@ String smsText, smsNumber;
 #define PhoneNumber  "+90**********" //  Write down the number of the phone that will communicate with the GSM shield, putting the country code in front of it.
 
 void setup() {
- /*
+
   digitalWrite(7, HIGH);
   delay(3000);
   digitalWrite(7, LOW);
- */
- // Delete the " /* and */ " above to automatically start the GSM shield
   Robonio.start();
   delay(2000);
   Serial.begin(9600);
