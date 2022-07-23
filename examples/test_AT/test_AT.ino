826c35ca1e4d6e3afee09163d0609575ec6a0f7d
Kerem Akçora <77489853+kerem-ce@users.noreply.github.com>
2022-07-23 22:10:37 +0300

    Update test_AT.ino

--- examples/test_AT/test_AT.ino
+++ examples/test_AT/test_AT.ino
@@ -2,12 +2,10 @@
 SoftwareSerial Robonio(10, 11); // RX, TX
 
 void setup() {
- /*
+
   digitalWrite(7, HIGH);
   delay(3000);
   digitalWrite(7, LOW);
- */
- // Delete the " /* and */ " above to automatically start the GSM shield
   Serial.begin(9600);
   Robonio.begin(9600);
 }
